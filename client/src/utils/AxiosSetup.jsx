9659b6eb5c2053a17d58bfec020db5fc9ec0235e
FahadPC <mumufahad@gmail.com>
2024-01-18 19:28:41 +0600

    Client env

--- client/src/utils/AxiosSetup.jsx
+++ client/src/utils/AxiosSetup.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_REACT_APP_BASE_URL;
+const baseURL = import.meta.env.VITE_REACT_APP_BASE_URL ?? import.meta.env.REACT_APP_BASE_URL;
 console.log(baseURL);
 // const baseURL = "http://localhost:8080/api";
 // const baseURL = "https://pokemon-buzzinga-server.onrender.com/api"
